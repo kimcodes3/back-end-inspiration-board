f236f2438ebe54764d7dcf6030fb732fb607e66f
Anh Huynh <avh2@uw.edu>
2023-06-26 13:54:44 -0700

    Created database and model files for board and card and imported models into create_app in init.py file

--- app/__init__.py
+++ app/__init__.py
@@ -19,6 +19,8 @@ def create_app():
 
     # Import models here for Alembic setup
     # from app.models.ExampleModel import ExampleModel
+    from app.models.board import Board
+    from app.models.card import Card
 
     db.init_app(app)
     migrate.init_app(app, db)
@@ -26,6 +28,7 @@ def create_app():
     # Register Blueprints here
     # from .routes import example_bp
     # app.register_blueprint(example_bp)
+    
 
     CORS(app)
     return app
--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,8 @@
 from app import db
+
+class Card(db.Model):
+    card_id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String)
+    likes_count = db.Column(db.Integer)
+    
+
